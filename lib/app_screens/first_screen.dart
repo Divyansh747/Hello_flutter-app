c9517e12aba22f65653ed2862e3368607a23c6b5
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-01-22 16:50:34 +0530

    Random_Number
    Random_number_generator_widget

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class FirstScreen extends StatelessWidget{
 @override
 Widget build(BuildContext context) {
  return Material(
-    color: Colors.lightGreenAccent,
+    color: Colors.cyan,
     child: Center(
       child: Text(
-        "HELLO FLUTTER",
+        generateLuckyNumber(),
         textDirection: TextDirection.ltr,
         style: TextStyle(color: Colors.white, fontSize: 42.0),
-      ), //text
-    ),
-  );
+      ),
+  ),
+ );
 }
+
+String generateLuckyNumber(){
+
+  var random = Random();
+  int luckyNumber = random.nextInt(10);
+
+  return "Your Lucky Number: $luckyNumber";
+}
+
 }
