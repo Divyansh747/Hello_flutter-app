cc468f92b2c29b8ca6c00a55613b141e5a43621c
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-01-23 00:01:17 +0530

    Add:LongList App
    This commit will add Long List app with debugPrint function

--- lib/main.dart
+++ lib/main.dart
@@ -6,47 +6,31 @@ void main() {
     debugShowCheckedModeBanner: false, //ToRemoveDebugLogo
     title: "Exploring UI widgets",
     home: Scaffold(
-      body: getListView(),
+      appBar: AppBar(title: Text("Long List"),),
+     body: getListView()
     ),
   ));
 }
 
-Widget getListView() {
+List<String> getListElements() {
+  var items = List <String>.generate(1000,(counter) => "Item $counter");
+  return items;
+  }
 
-  var listView = ListView(
-    children: <Widget>[
-
-      ListTile(
-        leading:  Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beautiful View !"),
-        trailing: Icon(Icons.wb_sunny),
-        onTap: () {
-          debugPrint("Landscape tapped");
+  Widget getListView() {
+    var listItems = getListElements();
+    var listView = ListView.builder(
+    itemBuilder: (context, index) {
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+      title: Text(listItems[index]),
+        onTap: (){
+          debugPrint('${listItems[index]} was tapped');
         },
-      ),
-
-      ListTile(
-        leading:  Icon(Icons.local_car_wash),
-        title: Text("Car Wash"),
-        subtitle: Text("Wash Your Car Here !"),
-        trailing: Icon(Icons.grain),
-        onTap: () {
-          debugPrint("Car Wash tapped");
-        },
-      ),
-
-      ListTile(
-        leading:  Icon(Icons.wifi),
-        title: Text("Wifi"),
-        subtitle: Text("Turn On Your Wifi"),
-        trailing: Icon(Icons.settings_applications),
-        onTap: () {
-          debugPrint("Turning On Wifi");
-        },
-      ),
-    ],
-  );
-
-return listView;
+      );
+  }
+    );
+  return listView;
 }
+
+
