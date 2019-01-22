c6f8dd7bdfd9b43ac5d26cf2e94dd10d735d0607
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-01-22 15:32:37 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+
+
+void main(){
+
+  runApp(
+      Center(
+      child: Text(
+          "HELLO FLUTTER", textDirection: TextDirection.ltr
+      )//text
+      )
+  );//runapp
+
+}
