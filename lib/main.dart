53864e6a0ca1d38b22397d5b4b30183d2f15e9cc
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-01-22 23:43:21 +0530

    Add:ListView App
    This commit will add Basic ListView App

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,52 @@
 import "package:flutter/material.dart";
-import './app_screens/first_screen.dart';
+
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false, //ToRemoveDebugLogo
     title: "Exploring UI widgets",
-    home: FirstScreen()
+    home: Scaffold(
+      body: getListView(),
+    ),
   ));
+}
+
+Widget getListView() {
+
+  var listView = ListView(
+    children: <Widget>[
+
+      ListTile(
+        leading:  Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View !"),
+        trailing: Icon(Icons.wb_sunny),
+        onTap: () {
+          debugPrint("Landscape tapped");
+        },
+      ),
+
+      ListTile(
+        leading:  Icon(Icons.local_car_wash),
+        title: Text("Car Wash"),
+        subtitle: Text("Wash Your Car Here !"),
+        trailing: Icon(Icons.grain),
+        onTap: () {
+          debugPrint("Car Wash tapped");
+        },
+      ),
+
+      ListTile(
+        leading:  Icon(Icons.wifi),
+        title: Text("Wifi"),
+        subtitle: Text("Turn On Your Wifi"),
+        trailing: Icon(Icons.settings_applications),
+        onTap: () {
+          debugPrint("Turning On Wifi");
+        },
+      ),
+    ],
+  );
+
+return listView;
 }
