0cdff1e72555ac64aa9264cbdbeac8ee403d4285
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-01-22 22:53:23 +0530

    Add:Assets_Column&Row
    This commit will add assets and column row feature in widgit

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,36 +1,79 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
-class FirstScreen extends StatelessWidget{
-@override
-Widget build(BuildContext context) {
- return Material(
-    //color: Colors.cyan,
-
-     child: Center(
-       child: Container(
-         alignment: Alignment.center,
-         color: Colors.cyan,
-         width: 380,
-         height: 100,
-       child: Text(
-         generateLuckyNumber(),
-         textDirection: TextDirection.ltr,
-         style: TextStyle(color: Colors.white, fontSize: 32.0),
-       ),
-     ),
-
-   )
-
- );
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+            padding: EdgeInsets.only(left: 10.0, top: 40.0),
+            alignment: Alignment.center,
+            color: Colors.blueAccent,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                          "Strawberry",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        )),
+                    Expanded(
+                        child: Text(
+                          "Banana",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                          "Mango",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        )),
+                    Expanded(
+                        child: Text(
+                          "Apple",
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(
+                              decoration: TextDecoration.none,
+                              fontSize: 35.0,
+                              fontFamily: 'Raleway',
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        )),
+                  ],
+                ),
+                PcImageAsset()
+              ],
+            )));
+  }
 }
 
-String generateLuckyNumber(){
+class PcImageAsset extends StatelessWidget {
 
-  var random = Random();
-  int luckyNumber = random.nextInt(10);
-
-  return "Your Lucky Number: $luckyNumber";
-}
-
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/pc.png');
+    Image image = Image(image: assetImage, width: 250.0, height: 250.0,);
+    return Container(child: image,);
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import "package:flutter/material.dart";
 import './app_screens/first_screen.dart';
 
-void main() => runApp(new MyFlutterApp());
-
-class MyFlutterApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "My Flutter App", //titleText
-        home: Scaffold(
-        appBar: AppBar(title: Text("Hello World !"),),
-    body: FirstScreen()
-
-    )
-    );
-  }
-
-
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, //ToRemoveDebugLogo
+    title: "Exploring UI widgets",
+    home: FirstScreen()
+  ));
 }
