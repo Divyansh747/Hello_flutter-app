8fdb95ddb7212c1337b5d88748a6b5355a01a889
Divyansh-747 <divyansh.1999.dr@gmail.com>
2019-01-22 15:48:34 +0530

    Adding Material Design
    This commit will add material design feel & title bar text

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,22 @@ import "package:flutter/material.dart";
 void main(){
 
   runApp(
-      Center(
-      child: Text(
-          "HELLO FLUTTER", textDirection: TextDirection.ltr
-      )//text
-      )
+
+      MaterialApp(
+      title: "My Flutter App", //titleText
+      home: Material(
+        color: Colors.lightGreenAccent,
+        child:  Center(
+            child: Text(
+                "HELLO FLUTTER",
+                textDirection: TextDirection.ltr,
+              style: TextStyle(color: Colors.white, fontSize: 42.0),
+            ),//text
+        ),
+      ),
+    )
+
+
   );//runapp
 
 }
